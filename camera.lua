128c118495556a7b9cfbbb07a42a15ff00055a7a
bjornenalfa <bjorn.detterfelt@telia.com>
2016-05-15 12:09:41 +0200

    show distance oob

--- camera.lua
+++ camera.lua
@@ -115,6 +115,8 @@ function camera.drawOOB()
     if i.activeEntity.x ~= x or i.activeEntity.y ~= y then
       love.graphics.setColor(255,255,255)
       love.graphics.draw(image.arrow, x, y, math.atan2(i.activeEntity.y-y, i.activeEntity.x-x), 2, 2, image.arrow:getWidth()/2, image.arrow:getHeight()/2)
+      love.graphics.setColor(255,0,0)
+      love.graphics.print(distance, x, y)
       --love.graphics.circle("fill", x, y, 10)
     end
   end
